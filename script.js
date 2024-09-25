71d96a290171ca734cedd5ede7061e48ae50a53d
Noémie Goosen <noemie.goosen@gmail.com>
2024-09-25 17:52:21 +0200

    feat: add grid generator

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+let gridSize = 16;
+
+const container = document.querySelector("main");
+
+function drawGrid() {
+  container.innerHTML = "";
+
+  for (let i = 0; i < gridSize; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+
+    for (let j = 0; j < gridSize; j++) {
+      const slot = document.createElement("div");
+      slot.classList.add("slot");
+      rowDiv.appendChild(slot);
+    }
+
+    container.appendChild(rowDiv);
+  }
+}
+
+drawGrid();
